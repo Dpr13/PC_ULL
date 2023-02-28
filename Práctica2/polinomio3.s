bb806252e4cd1e604eb1ac7524cddf1a7af58eaa
Daniel Pérez <110666246+Dpr13@users.noreply.github.com>
2023-02-28 18:05:20 +0000

    Update polinomio3.s

--- Práctica2/polinomio3.s
+++ Práctica2/polinomio3.s
@@ -80,47 +80,50 @@ do_while:
 #    std::cin >> s;
 	li	$v0,5
 	syscall
-	move	$s0,$v0
+	move	$s1,$v0
 #  } while (r > s);
-while:
-	blt $s0, $s1,while_fin
+	bgt $s0, $s1,for_fin
 #  for (int x = r ; x <= s ; x++) {
 	move $s2, $s0
 for:
-	bgt $s2, $s1, for_fin
-	addi $s2,1
 #    float f = a*x*x*x + b*x*x + c*x + d;
 	mtc1 $s2,$f0
 	cvt.s.w $f0,$f0
-	mul.s $f4,$f0,$f0 #f4 = x * x
-	mul.s $f6,$f4,$f0 #f6 = x * x * x
-	mul.s $f8,$f6,$f20 #f8 = x * x * x * a
-	mul.s $f10,$f22,$f4 #f10 = x * x * b
-	mul.s $f16,$f24,$f0 #f16 = x * c
-	add.s $f4,$f8,$f10 #f4 = x * x * b + x * x * x * x * a
-	add.s $f4,$f16,$f4 #f4 = x * x * b + x * x * x * x * a + x * c
-	add.s $f4,$f26,$f4 #f4 = x * x * b + x * x * x * x * a + x * c + d
+	mul.s $f4,$f0,$f0 #f4 = x*x
+	mul.s $f6,$f4,$f0 #f6 = x*x*x
+	mul.s $f8,$f6,$f20 #f8 = x*x*x*a
+	mul.s $f10,$f22,$f4 #f10 = x*x*b
+	mul.s $f16,$f24,$f0 #f16 = x*c
+	add.s $f4,$f8,$f10 #f4 = x*x*b + x*x*x*a
+	add.s $f4,$f16,$f4 #f4 = x*x*b + x*x*x*a + x*c
+	add.s $f4,$f26,$f4 #f4 = x*x*b + x*x*x*a + x*c + d
 	mov.s $f28, $f4
 #    if (f >= 2.1) {
 if_float:
 	li.s $f6,2.1
-	c.lt.s $f28,$f6,if_float_fin
+	c.lt.s $f28,$f6
+	bc1t if_float_fin
 #      std::cout << "\nf(" << x << ") = " << f;
 	li $v0,4
 	la $a0,strPrimerPar
 	syscall
 	li $v0,1
-	la $a0,$s2
+	move $a0,$s2
 	syscall
 	li $v0,4
 	la $a0,strSegundoPar
 	syscall
 	li $v0,2
-	la $a0,$f28
+	mov.s $f12,$f28
 	syscall
+	bgt $s2, $s1, for_fin
+	addi $s2,1
+	b for
 if_float_fin:
+	bgt $s2, $s1, for_fin
+	addi $s2,1
+	b for
 for_fin:
-while_fin:
 #  std::cout << "\n\nTermina el programa\n";
 	li	$v0,4
 	la	$a0,strFinPrograma
